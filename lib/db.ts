70b82b33ec47e0b17c42afe9ac91f6292818fde2
Julia <jlstreet@tamu.edu>
2026-03-27 22:48:36 -0500

    fixed login request for auth0

--- lib/db.ts
+++ lib/db.ts
@@ -3,4 +3,7 @@ import { Pool } from "pg";
 // used in the api folders for db queries
 export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });

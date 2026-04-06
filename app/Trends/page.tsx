8ba8778306eab78b17defc0ed1d7db52e5fd9a09
Matthew Hebert <matthew_hebert@tamu.edu>
2026-04-06 10:49:32 -0500

    worked more on trends

--- app/Trends/page.tsx
+++ app/Trends/page.tsx
@@ -15,22 +15,44 @@ import {
 import { use, useState } from "react";
 import { Line } from "react-chartjs-2";
 
+type tableEntry = {
+  id: number;
+  name: String;
+  frequency: number;
+};
+
 export default function TrendsPage() {
+  // create variables and functions to hold and set chart data
+  // pi chart for food and drink count sold in time period
+  const [piData, setPiData] = useState<any>(null);
+  // line chart of avg receipt purchase time in time period
+  const [timeData, setTimeData] = useState<any>(null);
+  // bar chart of monthly revenue and expense in time period
+  const [salesData, setSalesData] = useState<any>(null);
+  // line chart of number of receipts per month in time period
+  const [receiptData, setReceiptData] = useState<any>(null);
+  // Fill in data for weekly most frequently purchased table
+  const [tableEntry, setTableEntry] = useState<tableEntry[]>([]);
 
-    // create variables and functions to hold and set chart data
-    // pi chart for food and drink count sold in time period
-    const [piData, setPiData] = useState<any>(null);
-    // line chart of avg receipt purchase time in time period
-    const [timeData, setTimeData] = useState<any>(null);
-    // bar chart of monthly revenue and expense in time period
-    const [salesData, setSalesData] = useState<any>(null);
-    // line chart of number of receipts per month in time period
-    const [receiptData, setReceiptData] = useState<any>(null);
+  // loading variable to keep track of if still waiting on api request
+  // When set to false, safe to create graphs and display
+  const [loading, setLoading] = useState(true);
 
-    // loading variable to keep track of if still waiting on api request
-    // When set to false, safe to create graphs and display
-    const [loading, setLoading] = useState(true);
 
+
+
+  async function getWeeklyData() {
+    try {
+      // get weekly most frequently purchased items
+      const weeklyData = await fetch('http://localhost:3000/api/food-and-drink-count?weekly=true');
+
+      
+    }
+    catch (error) {
+      console.error(error.message);
+    }
     
-
+  }
+  
+  
 }
